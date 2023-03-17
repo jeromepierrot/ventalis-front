054b57a7a58f9c6d007ace6d31af7e2a5c42d803
jpierrot <je.pierrot@gmail.com>
2023-03-17 12:14:19 +0100

    feat/footer-header mod: move footer and add/change sticky attribute to footer and header TODO : to be applied to catalog and contact later + still FAB upward button to be fixed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'vtl-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
-  isTesting: boolean = true;
+export class AboutComponent implements OnInit {
+  isTesting!: boolean;
 
+  ngOnInit(): void {
+    this.isTesting = true;
+  }
+
+  onUpward() {
+    // TODO: Fix upward button
+    //this.router.navigateByUrl(this.router.url)
+
+  }
 }
